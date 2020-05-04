12a3980d614e7f84ba96459ddb705e2fa5e052fa
sander2324 <43219603+sander2324@users.noreply.github.com>
2020-05-04 16:02:49 +0200

    Setup App structure

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'extends': 'airbnb',
+  'env': {
+    'browser': true,
+  },
+  'rules': {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/destructuring-assignment': 'off',
+  },
+};
