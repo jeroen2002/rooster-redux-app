bc3185e5425de61c2240aba8fac981701028ef0c
Jeroen Bosgraaf <jwbosgraaf2@gmail.com>
2020-05-08 21:46:36 +0200

    dffsfsd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { registerRootComponent } from 'expo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function App() {
+
+function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>RoosterRedux!</Text>
+      <Text>Home!</Text>
     </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'red',
+          labelStyle: {
+            fontSize: 12,
+          },
+          tabStyle: {
+            width: 100,
+          },
+          activeTabStyle: {
+            backgroundColor: 'red',
+          },
+          style: {
+            backgroundColor: 'blue',
+          },
+
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} style={{ borderBottomColor: 'black', borderBottomWidth: 3 }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
