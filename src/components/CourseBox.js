de705d75adb0fb3f7f912899c9bf561f74574056
sander2324 <43219603+sander2324@users.noreply.github.com>
2020-05-11 21:17:43 +0200

    Add sample content in coursebox

--- src/components/CourseBox.js
+++ src/components/CourseBox.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import globalStyles from '../style/globals';
+import { PRIMARY_COLOR, LIGHT_TEXT } from '../style/contstants';
 
 import CustomText from './CustomText';
 
 
+const styles = StyleSheet.create({
+  courseSegments: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  courseSegment: {
+    marginLeft: 15,
+    marginTop: '6.25%',
+  },
+});
+
 function CourseBox() {
   return (
     <View style={globalStyles.box}>
-      <CustomText style={{ textAlign: 'center', marginTop: 35 }}>Hallo</CustomText>
+      <View style={styles.courseSegments}>
+        <View style={styles.courseSegment}>
+          <CustomText color={PRIMARY_COLOR}>13:15</CustomText>
+          <CustomText>14:00</CustomText>
+        </View>
+        <View style={styles.courseSegment}>
+          <CustomText weight="bold">NED</CustomText>
+          <CustomText color={LIGHT_TEXT}>GRN2.04</CustomText>
+        </View>
+      </View>
     </View>
   );
 }
--- src/style/contstants.js
+++ src/style/contstants.js
@@ -1,2 +1,3 @@
 export const PRIMARY_COLOR = '#5C6BC0';
 export const TEXT_COLOR = '#37474F';
+export const LIGHT_TEXT = '#53636B';
